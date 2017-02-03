a6798cd08985be2245dfd649d898526052d41aed
skylarhansen <skylarshansen@gmail.com>
2017-02-03 10:27:24 -0700

    added Spur model

---
+++ Spur.swift
@@ -0,0 +1,14 @@
+//
+//  Spur.swift
+//  Testing
+//
+//  Created by Skylar Hansen on 2/3/17.
+//  Copyright © 2017 Skylar Hansen. All rights reserved.
+//
+
+import Foundation
+
+struct Spur {
+    let name: String
+    let number: Int
+}
